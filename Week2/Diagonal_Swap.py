7c410d8837859ebc774003946d7c98122b8d1e39
Ronitjain30 <57800369+Ronitjain30@users.noreply.github.com>
2021-06-04 10:37:43 +0530

    Add files via upload

---
+++ Week2/Diagonal_Swap.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+print("Creating a square matrix :-")
+rows_and_columns = int(input("Enter the number of rows and columns : "))
+
+print("Enter the matrix values in a single line (separated by space) : ")
+
+matrix_values = list(map(int, input().split()))
+if (len(matrix_values) != rows_and_columns**2):
+    print("Total number of matrix values should be equal to", rows_and_columns**2)
+    exit()
+
+matrix = np.array(matrix_values).reshape(rows_and_columns, rows_and_columns)
+
+print("Input Matrix :-")
+print(matrix)
+
+array_of_diagonal = matrix.diagonal().tolist()
+
+np.fill_diagonal(matrix, np.fliplr(matrix).diagonal())
+np.fill_diagonal(np.fliplr(matrix), array_of_diagonal)
+
+print("Output Matrix :-")
+print(matrix)
